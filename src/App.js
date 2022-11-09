e8ed5276e6599790764ff4fe191aa1d9fdad3ac2
Payal Thummar <payalthummar1994@gmail.com>
2022-11-09 18:49:53 +0100

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import InputForm from "./components/inputform/InputForm";
 
 function App() {
-  return <div className="App">...</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <InputForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,9 @@
+export default function TodoList() {
+  return (
+    <div>
+      <div class="task-container">
+        <ul class="task-list"></ul>
+      </div>
+    </div>
+  );
+}
