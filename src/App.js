5b76e49a9dc4d7022e03697859baa4a1d67c2143
nirgluzman <nir.gluzman@gmail.com>
2022-11-10 19:12:16 +0100

    re-architect to input new task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import "./App.css";
-import Navbar from "./components/navbar/Navbar";
+import { useState } from "react";
+import Header from "./components/header/Header";
 import InputForm from "./components/inputform/InputForm";
+import Todo from "./components/Todo";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      task: "Give dog a bath",
+      complete: true,
+    },
+    {
+      id: 2,
+      task: "Do laundry",
+      complete: true,
+    },
+    {
+      id: 3,
+      task: "Vacuum floor",
+      complete: false,
+    },
+  ]);
+
+  const handleAddTodo = (task, id) => {
+    // setData((prevData) => {
+    //   return [...prevData, { id: id, task: task, complete: false }];
+    // });
+    console.log("function has run");
+  };
+
+  const [count, setCount] = useState(0);
+
+  function add() {
+    setCount((prev) => prev + 1);
+  }
+
   return (
     <div className="App">
-      <Navbar />
-      <InputForm />
+      <Header />
+      <main>
+        <InputForm handleAddTodo={handleAddTodo} />
+        <Todo data={data} />
+      </main>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="logo-image"></div>
+      <div className="app-title">
+        <h1>Organize your work and life with us!</h1>
+        <h2>make your life easier</h2>
+      </div>
+    </div>
+  );
+}
